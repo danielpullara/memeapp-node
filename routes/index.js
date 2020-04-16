8601171f2e2591174cb820d4626b806652034740
dannyp <danielpullara92@gmail.com>
2020-04-16 09:11:22 -1000

    duplicates feature & resizing feature

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var upload = require("../utils/upload");
-const {loadData, saveData} = require("../utils/data")
+const { loadData, saveData } = require("../utils/data")
+const jimp = require("jimp")
+const fs = require("fs")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,19 +11,40 @@ router.get('/', function (req, res, next) {
 });
 
 router.get("/browse", (req, res) => {
-  res.render("allimages")
+  const data = loadData()
+  res.render("allimages", { images: data })
 })
 
-router.post("/upload", upload.single("fileUpload"),(req, res) => {
-  console.log(req.file)
-  if(!req.file){
-    res.render("allimages", {error:"you need to upload a file"})
+router.post("/upload", upload.single("fileUpload"), async (req, res) => {
+  const { file } = req
+  if (!file) {
+    return res.render("index", { error: "you need to upload a file" })
   };
-const data = loadData()
-data.push(req.file)
-saveData(data)
+  const data = loadData()
+  const found = data.findIndex(el => el.originalname === file.originalname || el.size === file.size)
+  if (found !== -1) {
+    return res.render("index", { error: "file duplication" })
+  }
+  
+  try {
+    let image = await jimp.read(file.path)
+    image.resize(300, jimp.AUTO, jimp.RESIZE_NEAREST_NEIGHBOR);
+    await image.writeAsync(file.path);
+    
+    file.id = data.length === 0 ? 1 : [data.length - 1].id + 1;
+    data.push(file)
+    saveData(data)
+    
+    res.render("allimages", { images: data })
+  } catch (e) {
+    fs.unlinkSync(file.path);
+    return res.render("index", { error: e.message })
+  }
 
-  res.render("allimages", {images:data})
+
+
+
+ 
 })
 
 module.exports = router;
